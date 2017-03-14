40637f0ab9420c610cbe833d6059aae2a52ef4d0
SarahHale <id15she@cs.umu.se>
2017-03-14 14:44:45 +0100

    Session is starting to work

--- src/main/java/com/example/SpringPrototyp/Controllers/DelarController.java
+++ src/main/java/com/example/SpringPrototyp/Controllers/DelarController.java
@@ -3,9 +3,7 @@ package com.example.SpringPrototyp.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +11,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.SpringPrototyp.Components.Del;
 import com.example.SpringPrototyp.Components.DelRepository;
 
 
+
 @Controller
 @RequestMapping("/delar")
+@SessionAttributes("del")
 public class DelarController {
 	
 	@Autowired
@@ -40,10 +41,17 @@ public class DelarController {
 		return del;
 		
 	}
+	
 	@RequestMapping(value = "/{delNamn}",method = RequestMethod.GET)
-	@ResponseBody
-	public HttpEntity<Del> getDelTypes(@PathVariable("delNamn") String delNamn){
-		return new ResponseEntity<>(repository.findByName(delNamn), HttpStatus.OK);
+	public String getDelTypes(@PathVariable("delNamn") String delNamn,Model model){
+		System.out.println("here");
+		
+		if(repository.findByName(delNamn)!=null){
+		model.addAttribute("del", repository.findByName(delNamn));
+		}
+		System.out.println(model.containsAttribute("del"));
+		
+		return "/delar";
 	}
 
 }
--- src/main/java/com/example/SpringPrototyp/Controllers/TypController.java
+++ src/main/java/com/example/SpringPrototyp/Controllers/TypController.java
@@ -2,9 +2,7 @@ package com.example.SpringPrototyp.Controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,39 +10,45 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.SpringPrototyp.Components.Typ;
 import com.example.SpringPrototyp.Components.TypRepository;
 
 
 @Controller
+@SessionAttributes("del")
 @RequestMapping("/typer")
 public class TypController {
 
 	@Autowired
 	private TypRepository typRepository;
-	
+
 	@RequestMapping("")
 	@ResponseBody
 	public String fetchTyper(Model model){
 		model.addAttribute("typer", typRepository.findAll());
 		return "/delar";
-		
+
 	}
 	@RequestMapping( value = "/{typNamn}", method = RequestMethod.GET)
-	public HttpEntity<Typ> getInfoForTyp(@PathVariable("typNamn") String typNamn){
+	public String getInfoForTyp(@PathVariable("typNamn") String typNamn, Model model){
+		System.out.println("here");
+		if(typRepository.findByName(typNamn)!= null){
+			model.addAttribute("typer",typRepository.findByName(typNamn));
+		}
+		System.out.println(model.containsAttribute("typer"));
 		
-		Typ typ = typRepository.findByName(typNamn);
-		return new ResponseEntity<>(typ, HttpStatus.OK);
-		
+		return "/delar";
+
 	}
 	@RequestMapping(value = "/{name}", method = RequestMethod.PUT)
 	public @ResponseBody Typ updateTyp(@PathVariable("name") String name,
 			@RequestBody Typ typ){
 
 		typRepository.insert(typ);
-		
+
 		return typ;		
 	}
-	
+
 }
